323c246dfc14657339cf90952055f98ce9c5cb7b
ashishpathak128 <pathakashish128@yahoo.com>
2020-09-10 14:27:15 +0530

    Initial commit

---
+++ src/main/java/io/ashishworks/movieinfoservice/resoyrces/getMovieInfo.java
@@ -0,0 +1,11 @@
+package io.ashishworks.movieinfoservice.resoyrces;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class getMovieInfo {
+
+    public List<Movie> getMovie Info(int ){
+
+    }
+}
