a2917b90bad3bd09abc4229ee8488120d84d372a
ashishpathak128 <pathakashish128@yahoo.com>
2020-09-15 18:39:52 +0530

    Final implementation of API calls using RestTemplate

---
+++ src/main/java/io/ashishworks/movieinfoservice/resoyrces/Movie.java
@@ -0,0 +1,28 @@
+package io.ashishworks.movieinfoservice.resoyrces;
+
+public class Movie {
+
+    private String movieId;
+    private String name;
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public Movie(String movieId, String name) {
+        this.movieId = movieId;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/io/ashishworks/movieinfoservice/resoyrces/getMovieInfo.java
+++ src/main/java/io/ashishworks/movieinfoservice/resoyrces/getMovieInfo.java
@@ -1,11 +1,17 @@
 package io.ashishworks.movieinfoservice.resoyrces;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/movies")
 public class getMovieInfo {
 
-    public List<Movie> getMovie Info(int ){
-
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+        return new Movie(movieId,"test name");
     }
 }
